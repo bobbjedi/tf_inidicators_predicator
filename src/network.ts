ccc3edee28d386e8942502dc66f5f28624bc3521
bobbjedi <bobbraunjedi@gmail.com>
2021-11-18 17:09:27 +0300

    perc

--- src/network.ts
+++ src/network.ts
@@ -6,7 +6,7 @@ export const trainNet = async ({ symbol, tf, countCandels, testCount, callback }
   callback = callback || console.log
   let candels = await $u.getCandels('binance', symbol, countCandels, tf) //  баржа, пара, период, TF в минутах
   let i = 1
-  while (i++ < 2) {
+  while (i++ < 10) {
     console.log('End:', candels[0].open_time)
     candels = (await $u.getCandels('binance', symbol, countCandels, tf, candels[0].open_time * 1000)).concat(candels)
   }
@@ -14,7 +14,7 @@ export const trainNet = async ({ symbol, tf, countCandels, testCount, callback }
   const { set, lastInput } = $u.prepSet(candels)
   const trainingData = _.shuffle(set.slice(0, set.length - testCount).map(s => s.set))
 
-  const net = perceptronFromTFJS(trainingData[0].input.length, trainingData[0].output.length, [32, 16])
+  const net = perceptronFromTFJS([32])
   await net.trainNet({ data: trainingData, callback, epochs: 30 })
   console.log('net:', net)
   return { net, set, lastInput }
@@ -22,20 +22,30 @@ export const trainNet = async ({ symbol, tf, countCandels, testCount, callback }
 
 export type Log = { iterations: number, error: number}
 
-const perceptronFromTFJS = (input: number, output: number, hiddenLayers: number[], activation: any = 'relu') => {
+const perceptronFromTFJS = (hiddenLayers: number[], activation: any = 'relu') => {
   const model = tf.sequential()
-  model.add(tf.layers.dense({ inputShape: [input], units: hiddenLayers[0], activation }))
-  hiddenLayers.forEach((l, i) => model.add(tf.layers.dense({ inputShape: [l], units: hiddenLayers[i + 1] || output, activation })))
-  model.compile({ optimizer: tf.train.adadelta(0.01), loss: 'meanSquaredError' })
   return {
     async trainNet (opt: {data: {input:number[], output: number[]}[], callback: (d: Log)=> void, epochs: number}) {
+
+      const neurons = [opt.data[0].input.length].concat(hiddenLayers, [opt.data[0].output.length])
+      const layers = neurons.map((count, i) => {
+        return { inputShape: [count], units: neurons[i + 1], activation }
+      }).filter(l => l.units)
+      console.log({ neurons, layers })
+      layers.forEach(l => model.add(tf.layers.dense(l)))
+      model.compile({ optimizer: tf.train.adadelta(0.01), loss: 'meanSquaredError' })
+      console.log(model.layers)
+
       const x = tf.tensor2d(opt.data.map(s => s.input))
       const y = tf.tensor2d(opt.data.map(s => s.output))
-      for (let i = 1; i < 3; ++i) {
-        var h = await model.fit(x, y, { epochs: opt.epochs })
-        console.log('Loss after Epoch ' + i + ' : ' + h.history.loss[0])
-        opt.callback({ error: +h.history.loss[0], iterations: +h.epoch[0] })
-      }
+      await model.fit(x, y, {
+        epochs: opt.epochs,
+        callbacks: {
+          onEpochEnd (epoch, log) {
+            epoch > 3 && opt.callback({ error: log.loss, iterations: epoch })
+          }
+        }
+      })
     },
     run (input: number[]): number[] {
       return (model.predict(tf.tensor2d([input])) as any).arraySync()
