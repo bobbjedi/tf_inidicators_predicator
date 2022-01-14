e3a4d2f3f7d033c948a0bbe85cb0544af268abb1
citizenofneo <citizenofneo@gmail.com>
2022-01-14 22:49:42 +0300

    work test lstm

--- src/network.ts
+++ src/network.ts
@@ -1,8 +1,10 @@
 import * as tf from '@tensorflow/tfjs'
+import { input } from '@tensorflow/tfjs'
 import * as _ from 'underscore'
 import $u from './utils'
 
 export const trainNet = async ({ symbol, tf, countCandels, testCount, callback }: { symbol: string; tf: number, countCandels: number, testCount: number, callback?: (a: Log) => void }) => {
+  return
   callback = callback || console.log
   let candels = await $u.getCandels('binance', symbol, countCandels, tf) //  баржа, пара, период, TF в минутах
   let i = 1
@@ -56,16 +58,95 @@ const perceptronFromTFJS = (hiddenLayers: number[], activation: any = 'relu') =>
   // model.add(tf.layers.dense({ inputShape: [10], units: 1, activation }))
 }
 
-// XOR
+const lstm = (inputExample: number[][]) => {
+  const inputShape = tf.tensor2d(inputExample).shape
+  // const outputShape = tf.tensor1d(inputExample[0]).shape
+  console.log({ inputShape })
+  // console.log('input shape', inputShape)
+  const model = tf.sequential()
+  model.add(tf.layers.lstm({ inputShape, units: 64, returnSequences: true }))
+  // model.add(tf.layers.dropout({ rate: 0.2 }))
+  model.add(tf.layers.lstm({ units: 64, activation: 'relu' }))
+  // model.add(tf.layers.dropout({ rate: 0.2 }))
+  model.add(tf.layers.dense({ units: inputExample[0].length, activation: 'relu' }))
+  model.compile({ optimizer: tf.train.adam(0.1), loss: 'meanSquaredError' })
+  return model
+  // model.add(input)
+  // model.add(denseLayer1)
+  // model.add(denseLayer2)
+  // model.add(tf.layers.flatten())
+  // model.add(outputLayer)
+}
+
 setTimeout(async () => {
-  const set = [
-    { input: [0, 0], output: [0] },
-    { input: [1, 0], output: [1] },
-    { input: [0, 1], output: [1] },
-    { input: [1, 1], output: [0] },
-  ]
-  const net = perceptronFromTFJS([20, 20], 'relu')
-  const stat = await net.trainNet({ data: set, callback: console.log, epochs: 50 })
-  console.log('TFF', stat)
-  set.forEach(s => console.log(s.output[0], net.run(s.input)[0]))
+  const setdata = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]).map(e => e / 15)
+  const set = $u.separateArr($u.separateArr(setdata, 3), 3)
+  console.log(set)
+  const lstmNet = lstm(set[0])
+  console.log(':LL', lstmNet)
+
+  const inputs: number[][][] = []
+  const outputs: number[][] = []
+  set.forEach((input, i) => {
+    if (!set[i + 1]) {
+      return
+    }
+    inputs.push(input)
+    outputs.push(set[i + 1][2])
+  })
+  const x = tf.tensor3d(inputs)
+  const y = tf.tensor2d(outputs)
+  await lstmNet.fit(x, y, {
+    epochs: 100,
+    // batchSize: 1,
+    callbacks: {
+      onEpochEnd (epoch, log) {
+        epoch > 3 && console.log(epoch, log)
+      }
+    }
+  })
+  inputs.forEach((input, i) => {
+    const predict = (lstmNet.predict(tf.tensor3d([input])) as any).arraySync()[0] as number[]
+    console.log(input, predict.map(e => +(e * 15).toFixed(1)), outputs[i].map(e => e * 15))
+  })
 }, 1000)
+// const dataEl = opt.data[0]
+// const inputShape = tf.tensor2d(dataEl.input).shape
+// console.log('input shape', inputShape)
+// const input = tf.layers.dense({ inputShape, units: 32 })
+// const denseLayer1 = tf.layers.dense({ units: 64, activation: 'relu' })
+// const denseLayer2 = tf.layers.dense({ units: 128, activation: 'relu' })
+// const denseLayer3 = tf.layers.dense({ units: 32, activation: 'relu' })
+// const outputLayer = tf.layers.dense({ units: 1, activation: 'relu' })
+
+// model.add(input)
+// model.add(denseLayer1)
+// model.add(denseLayer2)
+// model.add(denseLayer3)
+// model.add(tf.layers.flatten())
+// model.add(outputLayer)
+// // Obtain the output symbolic tensor by applying the layers on the input.
+// // const output = denseLayer2.apply(denseLayer1.apply(input))
+
+// // Create the model based on the inputs.
+// // const model = tf.sequential({ inputs: input, outputs: output as tf.SymbolicTensor })
+
+// model.compile({ optimizer: tf.train.adadelta(0.01), loss: 'meanSquaredError' })
+// console.log(model.layers)
+// console.log('i>', opt.data.map(s => s.input))
+// const x = tf.tensor3d(opt.data.map(s => s.input))
+// const y = tf.tensor2d(opt.data.map(s => s.output))
+// await model.fit(x, y, {
+//   epochs: opt.epochs,
+//   batchSize: opt.batchSize || 12,
+//   callbacks: {
+//     onEpochEnd (epoch, log) {
+//       epoch > 3 && opt.callback({ error: log.loss, iterations: epoch })
+//     }
+//   }
+// })
+// },
+// run (input: number[][]): number[] {
+// return (model.predict(tf.tensor3d([input])) as any).arraySync()
+// }
+// }
